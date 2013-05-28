457a2aa2a58dd39fe1df52c5a605b0d1c3eef5dd
Alec Jen <jen.alec1994@gmail.com>
2013-05-28 16:28:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AlecApp::Application.config.secret_key_base = '48dc7e39f057d37412d8a958c657627d2e81a09247d6852e0b675ce8b1f5bfc00d5ba184435cc7ce0a226ef0192fc65df7384aef0b6ff6ddab4f0ba7cb065375'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AlecApp::Application.config.session_store :cookie_store, key: '_alec_app_session'
