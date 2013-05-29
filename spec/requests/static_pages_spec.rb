9999421c378fb949f625bcd655a02f1b5447793e
Alec Jen <jen.alec1994@gmail.com>
2013-05-28 17:31:13 -0700

    Finish static pages (3)

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+	
+	describe "Home page" do
+
+		it "should have the content 'myApp'" do
+			visit '/static_pages/home'
+			expect(page).to have_content('myApp')
+		end
+
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			expect(page).to have_title("Ruby on Rails Tutorial myApp | Home")
+		end
+	end
+
+	describe "Help page" do
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
+		end
+
+		it "should have the title 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_title("Ruby on Rails Tutorial myApp | Help")
+		end
+	end
+
+	describe "About page" do
+		it "should have the content 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_content('About Us')
+		end
+
+		it "should have the title 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_title("Ruby on Rails Tutorial myApp | About Us")
+		end
+	end
+
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("Ruby on Rails Tutorial myApp | Contact")
+		end
+	end
+end
