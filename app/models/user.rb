b2ed4a31683653547cd9368844a42757d10516ff
Alec Jen <jen.alec1994@gmail.com>
2013-06-23 01:53:05 -0700

    unique user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,12 @@ class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	before_save :create_remember_token
 	
-	validates :name, presence: true, length: { maximum: 50 }
+	validates :name, presence: true,
+					 length: { maximum: 50 }, 
+					 uniqueness: { case_sensitive: false }
+
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	
 	validates 	:email, presence: 	true, 
 						format: 	{ with: VALID_EMAIL_REGEX },
 						uniqueness: { case_sensitive: false }
